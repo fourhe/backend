651178598133029a00dd518bdcf8538cf97ef126
rkolx <olx.rko.o@gmail.com>
2023-10-27 21:03:03 +0900

    :heavy_plus_sign: WebClient 사용을 위해 설정 추가

---
+++ src/main/java/com/letter2sea/be/common/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.letter2sea.be.common.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create()
+            .resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+
+    @Bean
+    public WebClient webClient(HttpClient httpClient) {
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    }
+}
