b7c23f327a221784abfb4af9034e88af5259cc4b
rkolx <olx.rko.o@gmail.com>
2023-11-20 18:32:58 +0900

    :sparkles: 편지 랜덤하게 줍는 기능 구현
    - 더이상 주울 편지 없을시에 대한 예외처리는 추후 구현 예정
    - 추후 상황에 따라 동적쿼리 적용 예정

--- src/main/java/com/letter2sea/be/letter/controller/LetterController.java
+++ src/main/java/com/letter2sea/be/letter/controller/LetterController.java
@@ -2,33 +2,36 @@ package com.letter2sea.be.letter.controller;
 
 import com.letter2sea.be.auth.jwt.JwtProvider;
 import com.letter2sea.be.letter.dto.LetterCreateRequest;
-import com.letter2sea.be.letter.dto.LetterListResponse;
 import com.letter2sea.be.letter.service.LetterService;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
+import java.util.Collections;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/letter")
 public class LetterController {
 
     private final JwtProvider jwtProvider;
     private final LetterService letterService;
 
-    @PostMapping("/letter")
+    @PostMapping
     public void register(HttpServletRequest request,
         @RequestBody LetterCreateRequest letterCreateRequest) {
         Long writerId = jwtProvider.decode(request.getHeader("Authorization"));
         letterService.create(writerId, letterCreateRequest);
     }
 
-    @GetMapping("/letter")
-    public List<LetterListResponse> acquire(HttpServletRequest request) {
+    @GetMapping
+    public Map<String, Long> acquire(HttpServletRequest request) {
         Long memberId = jwtProvider.decode(request.getHeader("Authorization"));
-        return letterService.getRandom(memberId);
+        Long randomLetterId = letterService.getRandom(memberId);
+        return Collections.singletonMap("id", randomLetterId);
     }
 }
--- src/main/java/com/letter2sea/be/letter/service/LetterService.java
+++ src/main/java/com/letter2sea/be/letter/service/LetterService.java
@@ -5,20 +5,16 @@ import com.letter2sea.be.letter.dto.LetterCreateRequest;
 import com.letter2sea.be.letter.dto.LetterDetailResponse;
 import com.letter2sea.be.letter.dto.LetterListResponse;
 import com.letter2sea.be.letter.repository.LetterRepository;
-import com.letter2sea.be.mailbox.domain.MailBox;
 import com.letter2sea.be.member.Member;
 import com.letter2sea.be.member.repository.MemberRepository;
 import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -27,7 +23,6 @@ public class LetterService {
     private static final Random random = new SecureRandom();
     private final LetterRepository letterRepository;
     private final MemberRepository memberRepository;
-
 
 
     @Transactional
@@ -51,26 +46,27 @@ public class LetterService {
         return new LetterDetailResponse(findDetail);
     }
 
-    public List<LetterListResponse> getRandom(Long memberId) {
+    public Long getRandom(Long memberId) {
 
-        // 0, size()
-
-        Member member = memberRepository.findById(memberId)
-            .orElseThrow();
-
-        log.info("member id = {}", member.getId());
+        Member member = memberRepository.findById(memberId).orElseThrow();
 
         List<Long> readLetters = member.getMailBoxes().stream()
-            .map(mailBox -> member.getId())
+            .map(mailBox -> mailBox.getLetter().getId())
             .toList();
 
-        log.info("read letter ids = {}", Arrays.toString(readLetters.toArray()));
-        List<Letter> result = letterRepository.findAllByWriterNotAndIdNotIn(member, readLetters);
-
-        log.info("result = {}", Arrays.toString(result.toArray()));
-
-        return result.stream()
-            .map(LetterListResponse::new)
-            .toList();
+        if (readLetters.isEmpty()) {
+            List<Letter> unreadLetters = letterRepository.findAllByWriterNot(member);
+            if (unreadLetters.isEmpty()) {
+                throw new RuntimeException("더이상 읽을 편지가 없습니다.");
+            }
+            return unreadLetters.get(random.nextInt(unreadLetters.size())).getId();
+            //읽을 편지가 없다면 에러메시지를 응답값으로 전달 예정
+        }
+        List<Letter> unReadLetters = letterRepository.findAllByWriterNotAndIdNotIn(
+            member, readLetters);
+        if (unReadLetters.isEmpty()) {
+            throw new RuntimeException("더이상 읽을 편지가 없습니다.");
+        }
+        return unReadLetters.get(random.nextInt(unReadLetters.size())).getId();
     }
 }
