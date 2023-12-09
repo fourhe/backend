5666821a0e9d2a93da349d51791b7c3211373a64
PhilSoGooood <peelhw@gmail.com>
2023-12-09 19:07:28 +0900

    :sparkles: Validation 추가
    - 편지 작성 시 title, content NOT BLANK 처리

--- src/main/java/com/letter2sea/be/letter/dto/request/LetterCreateRequest.java
+++ src/main/java/com/letter2sea/be/letter/dto/request/LetterCreateRequest.java
@@ -2,11 +2,15 @@ package com.letter2sea.be.letter.dto.request;
 
 import com.letter2sea.be.letter.domain.Letter;
 import com.letter2sea.be.member.Member;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class LetterCreateRequest {
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
 
     public Letter toEntity(Member member) {
